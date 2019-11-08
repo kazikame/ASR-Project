7972184fe2c7699868cbfb86478eb84cc88bee0f
Yash Sharma <yash.sharma200999@gmail.com>
2019-11-08 21:45:25 +0530

    resample and npy

--- preprocess.sh
+++ preprocess.sh
@@ -4,3 +4,4 @@ mkdir wav 2> /dev/null
 for f in *.m4a; do ffmpeg -loglevel panic -ss 10 -t 10 -i $f "wav/${f/%m4a/wav}"; done
 cd wav
 for f in *.wav; do sox --norm $f "${f/%.wav/_mono.wav}" remix 1-2 && rm ${f}; done
+for x in `ls`; do sox $x -r 22050 "${x/%_mono.wav/_rs.wav}" && rm $x; done
