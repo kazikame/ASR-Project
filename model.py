cede99313ccabfae27a4962146ed1644d96b7275
Yash Sharma <yash.sharma200999@gmail.com>
2019-11-08 19:53:27 +0530

    start of run.ipynb

--- model.py
+++ model.py
@@ -1,27 +1,24 @@
 from keras.models import Sequential, Model
 from keras.layers import Dropout, MaxPooling2D, Convolution2D, Input, Lambda, concatenate, Flatten, Dense
-import logging
 from keras import backend as K
 from keras.losses import cosine_proximity
 from keras.optimizers import Adam
 
 params = {
         'dropout_factor': 0.5,
-        'n_dense': 0,
-        'n_dense_2': 0,
+        'n_dense': 1024,
+        'n_dense_2': 2048,
         'n_filters_1': 1024,
         'n_filters_2': 1024,
         'n_filters_3': 2048,
         'n_filters_4': 2048,
-        'n_filters_5': 0,
-        'n_kernel_1': (4, 96),
-        'n_kernel_2': (4, 1),
-        'n_kernel_3': (4, 1),
+        'n_kernel_1': (70, 4),
+        'n_kernel_2': (6, 4),
+        'n_kernel_3': (1, 4),
         'n_kernel_4': (1, 1),
-        'n_kernel_5': (1, 1),
         'n_out': '',
-        'n_pool_1': (4, 1),
-        'n_pool_2': (4, 1),
+        'n_pool_1': (1, 4),
+        'n_pool_2': (1, 4),
         'n_pool_3': (1, 1),
         'n_pool_4': (1, 1),
         'n_pool_5': (1, 1),
@@ -33,8 +30,8 @@ params = {
 }
 
 
-def get_model(input_shape, output_size):
-    inputs = Input(shape=input_shape)
+def get_model(input_shape1, input_shape2, output_size):
+    inputs = Input(shape=input_shape1)
 
     conv1 = Convolution2D(params["n_filters_1"], params["n_kernel_1"][0],
                           params["n_kernel_1"][1],
@@ -44,13 +41,13 @@ def get_model(input_shape, output_size):
                                        params["n_mel"]),
                           init="uniform")
     x = conv1(inputs)
-    # logging.debug("Input CNN: %s" % str(inputs.output_shape))
-    logging.debug("Output Conv2D: %s" % str(conv1.output_shape))
+    # print("Input CNN: %s" % str(inputs.output_shape))
+    print("Output Conv2D: %s" % str(conv1.output_shape))
 
     pool1 = MaxPooling2D(pool_size=(params["n_pool_1"][0],
                                     params["n_pool_1"][1]))
     x = pool1(x)
-    logging.debug("Output MaxPool2D: %s" % str(pool1.output_shape))
+    print("Output MaxPool2D: %s" % str(pool1.output_shape))
 
     x = Dropout(params["dropout_factor"])(x)
 
@@ -60,12 +57,12 @@ def get_model(input_shape, output_size):
                           activation='relu',
                           init="uniform")
     x = conv2(x)
-    logging.debug("Output Conv2D: %s" % str(conv2.output_shape))
+    print("Output Conv2D: %s" % str(conv2.output_shape))
 
     pool2 = MaxPooling2D(pool_size=(params["n_pool_2"][0],
                                     params["n_pool_2"][1]))
     x = pool2(x)
-    logging.debug("Output MaxPool2D: %s" % str(pool2.output_shape))
+    print("Output MaxPool2D: %s" % str(pool2.output_shape))
 
     x = Dropout(params["dropout_factor"])(x)
 
@@ -77,12 +74,12 @@ def get_model(input_shape, output_size):
                           activation='relu',
                           init="uniform")
     x = conv3(x)
-    logging.debug("Output Conv2D: %s" % str(conv3.output_shape))
+    print("Output Conv2D: %s" % str(conv3.output_shape))
 
     pool3 = MaxPooling2D(pool_size=(params["n_pool_3"][0],
                                     params["n_pool_3"][1]))
     x = pool3(x)
-    logging.debug("Output MaxPool2D: %s" % str(pool3.output_shape))
+    print("Output MaxPool2D: %s" % str(pool3.output_shape))
     x = Dropout(params["dropout_factor"])(x)
 
     conv4 = Convolution2D(params["n_filters_4"],
@@ -91,28 +88,30 @@ def get_model(input_shape, output_size):
                           activation='relu',
                           init="uniform")
     x = conv4(x)
-    logging.debug("Output Conv2D: %s" % str(conv4.output_shape))
+    print("Output Conv2D: %s" % str(conv4.output_shape))
 
     pool4 = MaxPooling2D(pool_size=(params["n_pool_4"][0],
                                     params["n_pool_4"][1]))
     x = pool4(x)
-    logging.debug("Output MaxPool2D: %s" % str(pool4.output_shape))
+    print("Output MaxPool2D: %s" % str(pool4.output_shape))
 
     x = Dropout(params["dropout_factor"])(x)
 
     flat = Flatten()
     x = flat(x)
-    logging.debug("Output Flatten: %s" % str(flat.output_shape))
+    print("Output Flatten: %s" % str(flat.output_shape))
 
-    # dense1 = Dense(output_dim=params["n_dense"], init="uniform", activation='linear')
-    # x = dense1(x)
-    # logging.debug("Output CNN: %s" % str(dense1.output_shape))
+    dense = Dense(output_dim=params["n_dense"], init="uniform", activation='linear')
+    x = dense(x)
+    # print("Output CNN: %s" % str(dense1.output_shape))
 
     # metadata
-    inputs2 = Input(shape=(params["n_metafeatures"],))
-    dense2 = Dense(output_dim=params["n_dense"], init="uniform", activation='relu')
-    x2 = dense2(inputs2)
-    logging.debug("Output CNN: %s" % str(dense2.output_shape))
+    inputs2 = Input(shape=input_shape2)
+    dense1 = Dense(output_dim=params["n_dense"], init="uniform", activation='relu')
+    x2 = dense1(inputs2)
+    dense2 = Dense(output_dim=params["n_dense_2"], init="uniform", activation='relu')
+    x2 = dense2(x2)
+    print("Output CNN: %s" % str(dense2.output_shape))
 
     x2 = Dropout(params["dropout_factor"])(x2)
 
@@ -121,12 +120,13 @@ def get_model(input_shape, output_size):
 
     dense3 = Dense(output_dim=output_size, init="uniform", activation='linear')
     xout = dense3(xout)
-    logging.debug("Output CNN: %s" % str(dense3.output_shape))
+    print("Output CNN: %s" % str(dense3.output_shape))
 
     lambda1 = Lambda(lambda x: K.l2_normalize(x, axis=1))
     xout = lambda1(xout)
 
     model = Model(input=[inputs, inputs2], output=xout)
+    model.compile(loss=cosine_proximity, optimizer=Adam, metrics=['accuracy'])
 
     return model
 
