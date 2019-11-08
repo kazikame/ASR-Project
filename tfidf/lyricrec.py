3da50d0882408497a65f6be98e9c92c446113128
pranayreddys <pranayr@cse.iitb.ac.in>
2019-11-08 18:15:59 +0530

    Added TF-IDF vector support

---
+++ tfidf/lyricrec.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pickle
+from sklearn.decomposition import TruncatedSVD
+from sklearn.random_projection import sparse_random_matrix
+import tensorflow as tf
+from tensorflow import keras
+import json
+pca_dim= 100
+with open('tfidf.pickle', 'rb') as f:
+	tf_idf_vector, tfidf_transformer, file_train_info, file_test_info, counts1, counts2=pickle.load(f)
+
+with open("songidToIndex.json") as json_file:
+    songind = json.load(json_file)
+
+with open("tosongs.json") as json_file:
+    tracktosong=json.load(json_file)
+
+
+song_factors=np.load('item_factors.npy')
+y=np.zeros([len(file_train_info), song_factors.shape[1]])
+for trackid, (_, countind) in file_train_info.items():
+	y[countind, :]=song_factors[songind[tracktosong[trackid][0]]]
+
+model = keras.Sequential([
+    keras.layers.Dense(80, input_shape=pca_dim, activation='relu'),
+    keras.layers.Dense(song_factors.shape[1], activation='relu')
+])
+
+
+
+model.compile(optimizer='adam',
+              loss= keras.losses.cosine_proximity,
+              metrics=['accuracy'])
+
+svd= TruncatedSVD(n_components=pca_dim, n_iter=10, random_state=42)
+svd.fit(tf_idf_vector)
+svd.transform(counts1)
+model.fit(svd.transform(counts1), y, epochs=10)
+
